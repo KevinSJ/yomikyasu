db114bc50224183a1154df9cdac04322c771e5d4
Kevin Jiang <KevinSJ@users.noreply.github.com>
2024-09-21 20:41:11 +1200

    Add printing when server starts

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"yomikyasu/internal/server"
 )
 
@@ -9,7 +10,9 @@ func main() {
 
 	server := server.NewServer()
 
+    log.Printf("Server will listen on %v ", server.Addr)
 	err := server.ListenAndServe()
+
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
 	}
