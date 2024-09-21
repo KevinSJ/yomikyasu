7a3541280b48529a53a070b7d943ca2f799be3d7
Kevin Jiang <KevinSJ@users.noreply.github.com>
2024-09-22 01:08:18 +1200

    Add tool packages

---
+++ internal/tool/feed_item_processor.go
@@ -0,0 +1,77 @@
+package tool
+
+import (
+	"log"
+
+	"cloud.google.com/go/texttospeech/apiv1/texttospeechpb"
+	"github.com/mmcdole/gofeed"
+)
+
+var VOICE_NAME_MAP_WAVENET = map[string]string{
+	"cmn-CN": "cmn-CN-Wavenet-A",
+	"en-US": "en-US-Neural2-C",
+}
+
+var VOICE_NAME_MAP_STANDARD = map[string]string{
+	"cmn-CN": "cmn-CN-Standard-D",
+	"en-US": "en-US-Standard-C",
+}
+
+func getSanitizedContentChunks(item *gofeed.Item) (textchunks []string) {
+	content := item.Title + "\n\n"
+
+	if len(item.Content) > 0 {
+		content += StripHtmlTags(item.Content)
+	} else if len(item.Description) > 0 {
+		content += StripHtmlTags(item.Description)
+	}
+
+	return ChunksByte(content, 5000)
+}
+
+func GetSynthesizeSpeechRequests(item *gofeed.Item, lang string, useNaturalVoice bool, speechSpeed float64) []*texttospeechpb.SynthesizeSpeechRequest {
+	contentChunks := getSanitizedContentChunks(item)
+
+	if len(lang) == 0 {
+		lang = GuessLanguageByUnicode(item.Title)
+	}
+
+	lang = GetSanitizedLanguageCode(lang)
+
+    log.Printf("lang: %v\n", lang)
+
+	languageName := VOICE_NAME_MAP_STANDARD[lang]
+	if useNaturalVoice {
+		languageName = VOICE_NAME_MAP_WAVENET[lang]
+	}
+
+    log.Printf("using voice %v for language code %v", languageName, lang)
+
+	synthesizeRequest := make([]*texttospeechpb.SynthesizeSpeechRequest, 0)
+
+	for _, chunk := range contentChunks {
+
+		req := texttospeechpb.SynthesizeSpeechRequest{
+			// Set the text input to be synthesized.
+			Input: &texttospeechpb.SynthesisInput{
+				InputSource: &texttospeechpb.SynthesisInput_Text{Text: chunk},
+			},
+			// Build the voice request, select the language code ("en-US") and the SSML
+			// voice gender ("neutral").
+			Voice: &texttospeechpb.VoiceSelectionParams{
+				LanguageCode: lang,
+				Name:         languageName,
+				SsmlGender:   texttospeechpb.SsmlVoiceGender_FEMALE,
+			},
+			// Select the type of audio file you want returned.
+			AudioConfig: &texttospeechpb.AudioConfig{
+				AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+				SpeakingRate:  speechSpeed,
+			},
+		}
+
+		synthesizeRequest = append(synthesizeRequest, &req)
+	}
+
+	return synthesizeRequest
+}
---
+++ internal/tool/string.go
@@ -0,0 +1,61 @@
+package tool
+
+import (
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+var JAPANESE_UNICODE_RANGE = []*unicode.RangeTable{
+	unicode.Hiragana, // Hiragana is the set of Unicode characters in script Hiragana.
+}
+
+var CHINESE_UNICODE_RANGE = []*unicode.RangeTable{
+	unicode.Han, // Han is the set of Unicode characters in script Han.
+}
+
+// Guess the language code for a string by looking at the unicode
+func GuessLanguageByUnicode(title string) string {
+	for _, c := range title {
+		if unicode.In(c, CHINESE_UNICODE_RANGE...) {
+			return "cmn-CN"
+		}
+	}
+	return "en-US"
+}
+
+func GetSanitizedLanguageCode(s string) string {
+	s2 := strings.Split(s, "-")
+
+	return s2[0] + "-" + strings.ToUpper(s2[len(s2)-1])
+}
+
+// returns the splited string by the size, chunkSize will be rounded to smallest
+// int divisble by the rune size
+func ChunksByte(s string, chunkSize int) []string {
+	if len(s) == 0 {
+		return nil
+	}
+
+	perRuneSize := len(s) / utf8.RuneCountInString(s)
+
+	if chunkSize <= perRuneSize || len(s) <= chunkSize {
+		return []string{s}
+	}
+	currentLen, currentStart := 0, 0
+
+	chunks := make([]string, 0)
+
+	for i, ch := range s {
+		if runeLen := utf8.RuneLen(ch); runeLen != -1 {
+			currentLen += runeLen
+			if currentLen > chunkSize {
+				chunks = append(chunks, s[currentStart:i])
+				currentLen = runeLen
+				currentStart = i
+			}
+		}
+	}
+
+	return append(chunks, s[currentStart:])
+}
