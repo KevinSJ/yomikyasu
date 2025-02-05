ac5813d888301802585511f88ad5be6b95fcc088
Kevin Jiang <KevinSJ@users.noreply.github.com>
2025-02-06 01:25:02 +1300

    fix: update view to include podcast information

--- db/migrations/007_create_view_podcasts_episodes.sql
+++ db/migrations/007_create_view_podcasts_episodes.sql
@@ -1,9 +1,10 @@
 -- +migrate Up
 -- 007_create_view_podcasts_episodes.sql
-CREATE VIEW IF NOT EXISTS podcasts_episodes AS SELECT podcast_id, episodes.*
+CREATE VIEW IF NOT EXISTS podcasts_episodes AS
+SELECT pf.podcast_id, episodes.*, podcasts.link as podcast_link, podcasts.description as podcast_description, podcasts.title as podcast_title
 FROM podcast_feed pf
-INNER JOIN episodes
-WHERE episodes.feed_id = pf.feed_id;
+INNER JOIN episodes ON pf.feed_id = episodes.feed_id
+INNER JOIN podcasts ON pf.podcast_id = podcasts.id;
 
 -- +migrate Down
 -- 007_create_view_podcasts_episodes.sql
